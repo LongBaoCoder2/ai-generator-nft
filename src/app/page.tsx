b348bdded2d0c16d9fbd48dc2bde292f7345ccb0
LongBaoCoder2 <longbao.it@gmail.com>
2024-08-10 17:45:40 +0700

    feat: add AIGenerator into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { ConnectEmbed } from "thirdweb/react";
 import { client } from "./client";
+import AIGenerator from "@/components/AIGenerator";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center gap-8 m-[20px]">
+
         <header className="font-bold text-5xl">
           <h1>
             AI Generator with NFT
@@ -13,6 +15,8 @@ export default function Home() {
         <main>
           <ConnectEmbed client={client}/>
         </main>
+
+        <AIGenerator />
     </div>
   );
 }
