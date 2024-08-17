d604bb204a14056d52cac59c40a813159fc9711a
LongBaoCoder2 <longbao.it@gmail.com>
2024-08-17 22:49:15 +0700

    feat(ui): NFTCollection.tsx

--- src/components/NFTCollection.tsx
+++ src/components/NFTCollection.tsx
@@ -1,25 +1,26 @@
 "use client";
 
 import { client } from "@/app/client";
-import React, {useState} from "react";
+import React from "react";
 import { NFT } from "thirdweb";
 import { MediaRenderer } from "thirdweb/react";
 
 type NFTCollectionProps = {
-    nfts: NFT[] | undefined
+    nfts: NFT[] 
 }
 
 const NFTCollection = ({ nfts } : NFTCollectionProps) => {
     return (
         <div className="flex flex-col items-center gap-5"> 
-            <h2 className="text-xl font-semibold">AI Generations:</h2>
+            <h2 className="text-xl font-semibold">AI Collection:</h2>
             
             <div className="grid grid-cols-4 gap-4">
                 {nfts && nfts.length > 0 ? nfts.map((nft) => (
                     <div key={nft.id} className="aspect-square relative">
-                    <MediaRenderer
-                        client={client}
-                    />
+                        <MediaRenderer
+                            client={client}
+                            src={nft.tokenURI}
+                        />
                     </div>
                 ))
                 : 
